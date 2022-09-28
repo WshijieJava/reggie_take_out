4f9950218b0b25f9805e6cf2e2960bfb5acda48f
wangshijie <2694956378@qq.com>
2022-09-28 23:30:30 +0800

    1

--- src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -102,4 +102,46 @@ public class ShoppingCartController {
 
         return R.success("清空购物车成功");
     }
+
+
+    @PostMapping("/sub")
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        log.info("购物车数据:{}", shoppingCart);
+
+        //设置用户id，指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+        //菜品id
+        Long dishId = shoppingCart.getDishId();
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+
+        if (dishId != null) {
+            //添加到购物车的是菜品
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            //添加到购物车的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+
+        //查询当前菜品或者套餐是否在购物车中
+        //SQL:select * from shopping_cart where user_id = ? and dish_id/setmeal_id = ?
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+
+        if (cartServiceOne != null && cartServiceOne.getNumber() != 0) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number - 1);
+            if (cartServiceOne.getNumber() > 0) {
+                shoppingCartService.updateById(cartServiceOne);
+            } else {
+                LambdaQueryWrapper<ShoppingCart> wrapper = new LambdaQueryWrapper<>();
+                wrapper.eq(cartServiceOne.getSetmealId()!=null,ShoppingCart::getSetmealId, cartServiceOne.getSetmealId());
+                wrapper.eq(cartServiceOne.getUserId()!=null,ShoppingCart::getUserId, cartServiceOne.getUserId());
+                wrapper.eq(cartServiceOne.getDishId()!=null,ShoppingCart::getDishId, cartServiceOne.getDishId());
+                shoppingCartService.remove(wrapper);
+            }
+        }
+        return R.success(cartServiceOne);
+    }
 }
